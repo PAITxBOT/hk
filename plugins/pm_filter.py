20e493dc6aa74bfbc9b7f127d343662a9bf15570
PAITxBOT <143090682+PAITxBOT@users.noreply.github.com>
2023-12-10 15:51:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,4 +1,4 @@
-import asyncio
+ import asyncio
 import re
 import ast
 import math
@@ -737,7 +737,7 @@ async def auto_filter(client, msg, spoll=False):
     btn.insert(0,
         [
             InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://telegram.me/LazyDeveloper'),
-            InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{req}")
+            InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{message.from_user.id}")
         ]
     )
 
